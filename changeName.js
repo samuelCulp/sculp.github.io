722b8b66e718326e376c42e160d5bb4ec68c7724
samuelCulp <99611615+samuelCulp@users.noreply.github.com>
2024-06-13 14:37:33 -0500

    Update changeName.js
    Added new table

--- changeName.js
+++ changeName.js
@@ -1,77 +1,216 @@
-function updateTable(category,department ) {
-    const table = document.getElementById('naming-convention-table');
+// Example data structure for department contacts
+const data = {
+    'ASKITD': [
+        { contact: ' ', email: ' ', phone: '(205) 996-5555', group: 'AskIT@uab.edu' },
+    ],
+    'SURGD': [
+        { contact: 'Demarkio Vincent | 934-6380', email: 'demarkio@uab.edu', phone: '', group: 'Surgeryit@uabmc.edu' },
+    ],
+    '1917D': [
+        { contact: 'Alfredo Guzman', email: 'aguzman@uabmc.edu', phone: '934-1212', group: '' },
+        { contact: 'Boniface Sindala', email: 'bsindala@uabmc.edu', phone: '', group: '' },
+        { contact: 'Micheal Henderson', email: 'reimann@uab.edu', phone: '', group: '' },
+    ],
+    'MainD': [
+        { contact: 'Johnny Rottenbery | Heritage Hall', email: ' ', phone: '', group: '' },
+        { contact: 'Kevin | Chemistry Building', email: ' ', phone: '', group: '' },
+    ],
+    'COLLATD': [
+        { contact: 'David Childers | 975-8872', email: 'childer@uab.edu', phone: '', group: '' },
+        { contact: 'Phillip Cotton | 975-8918', email: 'fly@uab.edu', phone: '', group: '' },
+    ],
+    'HSISIDD': [
+        { contact: 'Brian Day', email: 'bmday@uabmc.edu', phone: '', group: 'iamteam@uabmc.edu' },
+    ],
+    'OptoD': [
+        { contact: 'Chris Boutwell | 934-6567 | Dir Administrative Ops', email: 'cboutwel@uab.edu', phone: '', group: '' },
+        { contact: 'Harold \'Joey\' Jones | 934-2833 /cell: (205) 563-7609', email: 'hjjones@uab.edu', phone: '', group: '' },
+    ],
+    'SODD': [
+        { contact: 'Chris Walder | 975-7491', email: 'cklw@uab.edu', phone: '', group: '' },
+        { contact: 'Chris Campbell | 6-5417', email: 'jccampbe2001@uab.edu', phone: '', group: '' },
+        { contact: 'Joseph Lovetto | 934-5236', email: 'jlovetto@uab.edu', phone: '', group: '' },
+        { contact: 'Cedric Maddox | 975-3235', email: 'maddoxcl@uab.edu', phone: '', group: '' },
+    ],
+    'WBHMD': [
+        { contact: 'Darrell McCalla', email: 'dmcalla@uab.edu', phone: '934-9246', group: '' },
+    ],
+    'AOCD': [
+        { contact: 'Candance Barnes, IS Spec III | 731-9200', email: 'candancebarnes@alabamaorgancenter.org', phone: '', group: '' },
+    ],
+    'CallahanD': [
+        { contact: 'Chris Reid | (205) 325-8690 **ADOBE BUYER**', email: 'cgreid@uabmc.edu', phone: '325-8100', group: 'ophhelp@uab.edu' },
+        { contact: 'Erron McKinney | (205) 325-8336', email: 'emckinney@uabmc.edu', phone: '', group: '' },
+        { contact: 'Jack Rawls | (205) 327-6863 | **ADOBE BUYER**', email: 'jrawls@uabmc.edu', phone: '', group: '' },
+        { contact: 'James Wesson | (205) 325-8336 | **ADOBE BUYER**', email: 'jwesson@uabmc.edu', phone: '', group: '' },
+        { contact: 'Lacinda Riesland, Exec Dir IT Infrastruct', email: 'lriesland@uabmc.edu', phone: '', group: '' },
+        { contact: 'Theron Gobble', email: 'tgobble@uabmc.edu', phone: '', group: '' },
+        
+    ],
+    'DigitalD': [
+        { contact: 'Chris Humphries | 934-2374', email: 'chrishumphries@uab.edu', phone: '', group: '' },
+    ],
+    'HSISAssetD': [
+        { contact: 'Annette Jackson | **ADOBE BUYER**', email: 'annettejackson@uabmc.edu', phone: '', group: '' },
+        { contact: 'Geogia Cashion | 934-9234 | **ADOBE BUYER**', email: 'gcashion@uabmc.edu', phone: '', group: '' },
+        { contact: 'Lisa Shelton | **ADOBE BUYER**', email: 'lhshelton@uabmc.edu', phone: '', group: '' },
+    ],
+    'HuntsvilleD': [
+        { contact: 'James Baker (manager)', email: ' ', phone: '(256) 551-4482, (256) 824-3333', group: 'hsvhelp@uabmc.edu or helpdesk@uah.edu' },
+        { contact: 'Jennifer Childress', email: ' ', phone: '', group: '' },
+        { contact: 'Valorie Williams Spvsr - Material Mgmt | (256) 551-4525', email: ' ', phone: '', group: '' },
+
+    ],
+    'PathD': [
+        { contact: 'Israel Ponce-Rodriguez | 934-6610 | **ADOBE BUYER**', email: 'israel@uab.edu', phone: '', group: 'pathis@uab.edu' },
+    ],
+    'SOED': [
+        { contact: 'Jeremy Hicks | 934-8455', email: 'jhicks07@uab.edu', phone: '', group: '' },
+        { contact: 'Tommy Foley', email: 'tfoley@uab.edu', phone: '', group: '' },
+
+    ],
+    'AnesthesiaD': [
+        { contact: 'Noah Crum, IS Spec II | 975-2919 or 934-9650', email: 'ncrum@uabmc.edu', phone: '', group: '' },
+    ],
+    'CCTSFD': [
+        { contact: 'Alan Cook, IT Director', email: 'acook40@uab.edu', phone: '975-8240', group: 'cctsf@uab.edu' },
+        { contact: 'Elbert Jenkins, Consultant', email: 'elj@uab.edu', phone: '', group: '' },
+        { contact: 'Wesley Pate', email: 'wpate@uab.edu', phone: '', group: '' },
+
+    ],
+    'DOMD': [
+        { contact: 'Ahmad Khan', email: 'akkhan@uabmc.edu', phone: '975-4357', group: 'DOMHelp@uab.edu' },
+        { contact: 'Bryan Duke | 975-8662 | (205)-601-1607', email: 'bryanduke@uabmc.edu', phone: '', group: '' },
+        { contact: 'Cameron Davis', email: 'cameronrdavis@uabmc.edu', phone: '', group: '' },
+        { contact: 'Craig Mosley', email: 'crmosley@uabmc.edu', phone: '', group: '' },
+        { contact: 'Derek Gettis | **ADOBE BUYER**', email: 'dgettis@uabmc.edu', phone: '', group: '' },
+        { contact: 'Hunter James Vines', email: 'hunterjvines@uabmc.edu', phone: '', group: '' },
+        { contact: 'Jacob Ellis', email: 'jahkusbedell@uabmc.edu', phone: '', group: '' },
+        { contact: 'Jahkus Bedell', email: 'jaylenking@uabmc.edu', phone: '', group: '' },
+        { contact: 'Jamel Hardware', email: 'jellis@uabmc.edu', phone: '', group: '' },
+        { contact: 'Jaylen King | **ADOBE BUYER**', email: 'jhardware@uabmc.edu', phone: '', group: '' },
+        { contact: 'Jonathan Ingram', email: 'jingram@uabmc.edu', phone: '', group: '' },
+        { contact: 'Nazmul Islam | 934-3616', email: 'nislam@uabmc.edu', phone: '', group: '' },
+    ],
+    'HSISHDD': [
+        { contact: '', email: ' ', phone: '(205) 934-8888', group: 'helpdesk@uabmc.edu' },
+    ],
+    'NeuroD': [
+        { contact: 'Peter Michael Allendorfer | 123-4567', email: 'pmalle24@uab.edu', phone: '', group: '' },
+        { contact: 'Gary Spradley, ISSpec III | cel: 369-4603', email: 'spradley@uab.edu', phone: '', group: '' },
+    ],
+    'PhysicalD': [
+        { contact: 'Vicki Farris | 934-5049', email: 'vfarris@uabmc.edu', phone: '', group: '' },
+    ],
+    'TvD': [
+        { contact: ' ', email: ' ', phone: '934-6636', group: '' },
+    ],
+    'BiochemD': [
+        { contact: 'Gary Spradley, IS Spec II | cell: 369-4603', email: 'pmalle24@uab.edu', phone: '934-4733', group: '' },
+        { contact: 'Peter Michael Allendorfer | 123-4567', email: 'spradley@uab.edu', phone: '', group: '' },
+
+    ],
+    'FacilitiesD': [
+        { contact: 'Clay Boyce | 934-2825 cell 410-3435', email: 'cboyce@uab.edu', phone: '934-2825', group: 'https://www.uab.edu/facilities/information-technology-it' },
+        { contact: 'Craig Bowen', email: 'craigb@uab.edu', phone: '934-2825', group: '' },
+        { contact: 'Dave Gunnells | 934-2489 or 934-2487', email: 'gunnells@uab.edu', phone: '', group: '' },
+        { contact: 'Jeremy Crown', email: ' ', phone: '', group: '' },
+    ],
+    'HSISSYSD': [
+        { contact: 'Brian Edwords', email: 'brianpedwards@uabmc.edu', phone: '934-8888', group: 'desktopdev@uabmc.edu' },
+        { contact: 'Eric Fitts | 934-1189', email: 'efitts@uabmc.edu', phone: '', group: '' },
+        { contact: 'Ian McCandless | 975-3839', email: 'imccandless@uabmc.edu', phone: '', group: '' },
+        { contact: 'John Troyer | 975-357', email: 'jjjoyce@uabmc.edu', phone: '', group: '' },
+        { contact: 'Joseph Joyce Desktop | 975-0383', email: 'jtroyer@uabmc.edu', phone: '', group: '' },
+        { contact: 'Martin Donohue, Dir-Info Technology Svcs-Ent | 975-3816', email: 'modom@uabmc.edu', phone: '', group: '' },
+        { contact: 'Me\'Chelle Odom Desktop | 975-3838', email: 'modom@uabmc.edu', phone: '', group: '' },
+        { contact: 'Patricia Moore | 934-0084', email: 'pmoore@uabmc.edu', phone: '', group: '' },
+        { contact: 'Steve Prokop | 975-3582', email: 'sprokop@uabmc.edu', phone: '', group: '' },
+
+    ],
+    'OBGYND': [
+        { contact: 'Chamblin, Bette', email: 'bchamblin@uabmc.edu', phone: '', group: '' },
+        { contact: 'Nance, Harry | 934-7961', email: 'hjnance@uab.edu', phone: '', group: '' },
+        { contact: 'Purvis, Tim | 996-6156', email: 'tpurvis@uabmc.edu', phone: '', group: '' },
+
+
+    ],
+    'RepairCommD': [
+        { contact: 'REPAIRCOMM', email: ' ', phone: '975-1805', group: '' },
+    ],
+    'VivaD': [
+        { contact: 'Brandon Studdard', email: 'bstuddard@uabmc.edu', phone: '', group: 'vivahelpdesk@uabmc.edu' },
+        { contact: 'Gary Grayson', email: 'ggrayson@uabmc.edu', phone: '', group: '' },
+        { contact: 'Josh Hill', email: ' ', phone: 'jbhill@uabmc.edu', group: '' },
+        { contact: 'Steven Sebastian | 558-3181', email: 'ssebastian@uabmc.edu', phone: '', group: '' },
+
+    ],
     
-    // Clear existing table content
+
+    
+
+    // Add more departments as needed
+};
+
+const namingConventions = {
+    'ASKIT': ['AQA-', 'ARENA-', 'ARP-', 'ASC-', 'ASKIT-', 'ATH-', 'AUDIT-', 'AV-', 'ALC-', 'BFS-',
+    'BNVF-', 'CIRB-', 'COMM-', 'FA-', 'FINAD-', 'GEN-', 'GRAD-', 'HRM-', 'IACUC-', 'ID-', 'INTO-',
+    'IRB-', 'ITBFS-', 'ITDS-', 'ITIS-', 'LEGAL-', 'LHL-', 'LIB-', 'LRC-', 'OSP-', 'PAGE-', 'PROV-',
+    'PSEC-', 'RCOMP-', 'RF-', 'ROTC-', 'SEC-', 'SFS-', 'SHRP-', 'SHWC-', 'SIM-', 'SOD-', 'SOE-', 'SON-',
+    'SOPH-', 'TUCC-', 'UABPD-', 'UHP-', 'UTH-', 'VPDA-', 'VPED-', 'VPIT-', 'VPR-', 'VPSA-', 'WEBB-',
+    'WHSE-', ],
+    'Surg': ['SURG'],
+    '1917': ['blank'],
+    'Maintenance': ['blank'],
+    'COLLAT': ['blank'],
+    'HSISID': ['blank'],
+    'Optometry': ['SO'],
+    'SOD': ['blank'],
+    'WBHM': ['blank'],
+    'AOC': ['blank'],
+    'Callahan': ['OPH'],
+    'Digital': ['blank'],
+    'HSISAsset': ['blank'],
+    'Huntsville': ['blank'],
+    'Path': ['PATH'],
+    'SOE': ['blank'],
+    'Anesthesia': ['ANES'],
+    'CCTSF': ['RAD', 'PHAR', 'PHARM', 'PHARMTOX', 'SURG', 'WTI'],
+    'DOM': ['NEURO-', "PBN-", "PULM-", "RHEUM-", "SURGD-", "SURG-(if Children's ambulatory)", "URO-", "U*", "W*-LW-",
+            "CARD-", "CCTS-", "CDIB-", "CFRC-", "CNC-", "DOM-", "END-", "ENT-", "GEHEP-", "GGMPC-", "HEM-", "HEMONC-", "IDD-", 
+               "II-", "INTMD-", "NEPH-"],
+    'HSISBA': ['blank'],
+    'ICO': ['blank'],
+    'PEDS': ['EN','NE', 'NN', 'NH', 'OR', 'LOW'],
+    'MEIS': ['NSURG'],
+    'AV': ['blank'],
+    'CASIT': ['CAS'],
+    'DOPM': ['blank'],
+    'HSISHD': ['blank'],
+    'Neuro': ['Initials-Micro-lab# or Initials-Neuro-', 'NBIO'],
+    'Physical': ['blank'],
+    'TvRepair': ['blank'],
+    'Facilities': ['OHS'],
+    'HSISSYS': ['blank'],
+    'OBGYN': ['blank'],
+    'RepairComm': ['blank'],
+    'Viva': ['blank'],
+};
+
+function updateTable(category, department) {
+    updateNamingConventionTable(category);
+    updateDepartmentContactTable(department);
+}
+
+function updateNamingConventionTable(category) {
+    const table = document.getElementById('naming-convention-table').querySelector('tbody');
     table.innerHTML = '';
 
-    // Define naming conventions for each category
-    const namingConventions = {
-        'ASKIT': ['AQA-', 'ARENA-', 'ARP-', 'ASC-', 'ASKIT-', 'ATH-', 'AUDIT-', 'AV-', 'ALC-', 'BFS-',
-         'BNVF-', 'CIRB-', 'COMM-', 'FA-', 'FINAD-', 'GEN-', 'GRAD-', 'HRM-', 'IACUC-', 'ID-', 'INTO-',
-          'IRB-', 'ITBFS-', 'ITDS-', 'ITIS-', 'LEGAL-', 'LHL-', 'LIB-', 'LRC-', 'OSP-', 'PAGE-', 'PROV-',
-           'PSEC-', 'RCOMP-', 'RF-', 'ROTC-', 'SEC-', 'SFS-', 'SHRP-', 'SHWC-', 'SIM-', 'SOD-', 'SOE-', 'SON-',
-            'SOPH-', 'TUCC-', 'UABPD-', 'UHP-', 'UTH-', 'VPDA-', 'VPED-', 'VPIT-', 'VPR-', 'VPSA-', 'WEBB-',
-             'WHSE-', ],
-        'Surg': ['SURG'],
-        '1917': ['blank'],
-        'Maintenance': ['blank'],
-        'COLLAT': ['blank'],
-        'HSISID': ['blank'],
-        'Optometry': ['SO'],
-        'SOD': ['blank'],
-        'WBHM': ['blank'],
-        'AOC': ['blank'],
-        'Callahan': ['OPH'],
-        'Digital': ['blank'],
-        'HSISAsset': ['blank'],
-        'Huntsville': ['blank'],
-        'Path': ['PATH'],
-        'SOE': ['blank'],
-        'Anesthesia': ['ANES'],
-        'CCTSF': ['RAD', 'PHAR', 'PHARM', 'PHARMTOX', 'SURG', 'WTI'],
-        'DOM': ['NEURO-', "PBN-", "PULM-", "RHEUM-", "SURGD-", "SURG-(if Children's ambulatory)", "URO-", "U*", "W*-LW-",
-                  "CARD-", "CCTS-", "CDIB-", "CFRC-", "CNC-", "DOM-", "END-", "ENT-", "GEHEP-", "GGMPC-", "HEM-", "HEMONC-", "IDD-", 
-                     "II-", "INTMD-", "NEPH-"],
-        'HSISBA': ['blank'],
-        'ICO': ['blank'],
-        'PEDS': ['EN','NE', 'NN', 'NH', 'OR', 'LOW'],
-        'MEIS': ['NSURG'],
-        'AV': ['blank'],
-        'CASIT': ['CAS'],
-        'DOPM': ['blank'],
-        'HSISHD': ['blank'],
-        'Neuro': ['Initials-Micro-lab# or Initials-Neuro-', 'NBIO'],
-        'Physical': ['blank'],
-        'TvRepair': ['blank'],
-        'TvRepair': ['blank'],
-        'Facilities': ['OHS'],
-        'HSISSYS': ['blank'],
-        'OBGYN': ['blank'],
-        'RepairComm': ['blank'],
-        'Viva': ['blank'],
+    const names = namingConventions[category] || [];
 
-    };
-    const data = {
-        'ASKIT': [
-            {contact: 'Alfredo Guzman', email: 'alfredo@example.com', dept: 'Central IT', phone: '123-456-7890', group: 'Group A'},
-            {contact: 'Boniface Sindala', email: 'boniface@example.com', dept: 'Central IT', phone: '234-567-8901', group: 'Group B'},
-            {contact: 'Michael Henderson', email: 'michael@example.com', dept: 'Central IT', phone: '345-678-9012', group: 'Group C'}
-        ],
-        'Surg': [
-            {contact: 'Jane Doe', email: 'jane@example.com', dept: 'Surgery', phone: '456-789-0123', group: 'Group D'},
-            {contact: 'John Smith', email: 'john@example.com', dept: 'Surgery', phone: '567-890-1234', group: 'Group E'}
-        ],
-        // Add more departments and their data as needed
-    };
-
-    // Get the appropriate naming conventions for the selected category
-    const names = namingConventions[category] || [] ;
-
-    // Determine the number of rows and columns (e.g., 5 rows, 6 columns)
-    var rows = 9;
+    const rows = 9;
     const columns = 7;
 
-    // Create table rows and cells
     let nameIndex = 0;
     for (let i = 0; i < rows; i++) {
         const tr = document.createElement('tr');
@@ -83,20 +222,19 @@ function updateTable(category,department ) {
         }
         table.appendChild(tr);
     }
-    
+}
 
+function updateDepartmentContactTable(department) {
+    const tableBody = document.querySelector('#department-contact-table tbody');
+    tableBody.innerHTML = '';
 
-    const tableBody = document.querySelector('#naming-convention-table tbody');
-    tableBody.innerHTML = ''; // Clear previous table content
-
-    var rows = data[department];
+    const rows = data[department];
     if (rows) {
         rows.forEach(row => {
             const tr = document.createElement('tr');
             tr.innerHTML = `
                 <td>${row.contact}</td>
                 <td>${row.email}</td>
-                <td>${row.dept}</td>
                 <td>${row.phone}</td>
                 <td>${row.group}</td>
             `;
@@ -106,4 +244,4 @@ function updateTable(category,department ) {
 }
 
 // Initialize with default table
-window.onload = () => updateTable('ASKIT');
+window.onload = () => updateTable('ASKIT', 'ASKITD'); // Provide a department as needed
